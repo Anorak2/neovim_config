274155503b963dbf3ca73b46d5e70a4428a9c31f
Anorak2 <adamcoleberry@gmail.com>
2024-12-07 18:28:44 -0600

    made it easy to replace text

--- lua/anorak/remap.lua
+++ lua/anorak/remap.lua
@@ -1,2 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+
+-- Map Ctrl-r in visual mode to perform substitution on visually selected text
+vim.api.nvim_set_keymap('v', '<C-r>', '"hy:%s/<C-r>h//gc<left><left><left>', { noremap = true})
