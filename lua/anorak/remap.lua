66d88e7d09c260faedfd7bd5c2ad45dfe5a7bc9b
Anorak2 <adamcoleberry@gmail.com>
2025-09-20 20:46:02 -0500

    forcing myself to stop relying on the arrow keys, its a bad habit

--- lua/anorak/remap.lua
+++ lua/anorak/remap.lua
@@ -7,5 +7,10 @@ vim.api.nvim_set_keymap('v', '<C-r>', '"hy:%s/<C-r>h//gc<left><left><left>', { n
 
 
 -- Toggle 'wrap' setting with <leader>ww
-declare i32 @atoi(i8*)
 vim.api.nvim_set_keymap('n', '<leader>ww', ':set wrap!<CR>', { noremap = true, silent = true })
+
+-- Forcing myself to get better with hjkl
+vim.api.nvim_set_keymap('n', '<Up>', '<Nop>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Down>', '<Nop>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Left>', '<Nop>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Right>', '<Nop>', { noremap = true })
